ad252689df9edac4d086e02164abd4e3a0362e2f
Chris Pittman <chrispittman73@gmail.com>
2023-08-09 09:10:25 -0500

    add LTI XML configuration

--- web/routes/web.php
+++ web/routes/web.php
@@ -31,3 +31,4 @@ Route::middleware(['oauth'])->group(function() {
 });
 
 Route::post('/lti_launch', [PeerGradingController::class, 'ltiLaunch']);
+Route::get('/lti_xml', [PeerGradingController::class, 'getLTIXML']);
